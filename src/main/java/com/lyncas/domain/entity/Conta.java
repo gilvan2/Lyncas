49933f74e0e31ab12c63efa6236c40d45c8f2967
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-09-03 03:34:22 -0300

    Criação da entidade Conta

---
+++ src/main/java/com/lyncas/domain/entity/Conta.java
@@ -0,0 +1,81 @@
+package com.lyncas.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "contas_a_pagar")
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private LocalDate dataVencimento;
+
+    private LocalDate dataPagamento;
+
+    @NotNull
+    private BigDecimal valor;
+
+    @NotNull
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Situacao situacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public LocalDate getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(LocalDate dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    enum Situacao {
+        PENDENTE, PAGO, ATRASADO
+    }
+}
+
