8b699710aa39f6a756af3fd51ce529cf8f35421b
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-09-03 16:10:18 -0300

    inclusão da gestão do banco de dados para o Flyway

--- src/main/resources/db/migration/V1__CriarTabelaContasAPagar.sql
+++ src/main/resources/db/migration/V1__CriarTabelaContasAPagar.sql
@@ -1,10 +1,9 @@
-\c postgres;
-
+-- V1__CriarTabelaContasAPagar.sql
 CREATE TABLE IF NOT EXISTS contas_a_pagar (
     id SERIAL PRIMARY KEY,
     data_vencimento DATE NOT NULL,
     data_pagamento DATE,
     valor DECIMAL(10, 2) NOT NULL,
     descricao VARCHAR(255) NOT NULL,
-    situacao VARCHAR(50) NOT NULL CHECK (situacao IN ('Pendente', 'Pago', 'Atrasado'))
+    situacao VARCHAR(50) NOT NULL CHECK (situacao IN ('PENDENTE', 'PAGO', 'ATRASADO'))
 );
